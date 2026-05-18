d2f989b16f332dc54139ad9cb99b4f5179c62095
coder9988 <jainmudit616@gmail.com>
2026-05-18 23:37:54 +0530

    Add automatic superuser creation for Render deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,14 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username='${DJANGO_SUPERUSER_USERNAME}';
+email='${DJANGO_SUPERUSER_EMAIL}';
+password='${DJANGO_SUPERUSER_PASSWORD}';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
+
 exec gunicorn issue_tracker.wsgi:application --bind "0.0.0.0:${PORT:-8000}"
